dbc4214ac6b17c1fb499f7716171ed12e404a7a5
Clement Azalbert <clementazalbert04@gmail.com>
2022-09-29 17:26:41 +0200

    added attribute binding on img tag

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -10,12 +10,13 @@ export class FaceSnapComponent implements OnInit {
   description!: string;
   createdDate!: Date;
   snaps!: number;
+  imageUrl!: string;
 
   ngOnInit() {
-    this.title = "Mr.Alderson";
-    this.description = "My best friend";
+    this.title = "Sahara Dune";
+    this.description = "Great Holidays in the Sahara";
     this.createdDate = new Date();
     this.snaps = 4;
+    this.imageUrl = "https://i.picsum.photos/id/261/200/200.jpg?hmac=4mNici_jSM7rVZzJL7M6G24f0axnDV3BZR-LBo5gMIg";
   }
-
 }
