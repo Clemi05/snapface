dd82b08a8dacc2784efcece4d20a70e51c4e2b74
Clement Azalbert <clementazalbert04@gmail.com>
2022-09-30 16:03:19 +0200

    created personnalised props using OnInit and @Input()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      "Sahara Dune",
+      "Great Holidays in the Sahara",
+      new Date(),
+      0,
+      "https://i.picsum.photos/id/261/200/200.jpg?hmac=4mNici_jSM7rVZzJL7M6G24f0axnDV3BZR-LBo5gMIg"
+    )
+
+    this.myOtherSnap = new FaceSnap(
+      "Montains",
+      "Montains in the winter 2020",
+      new Date(),
+      0,
+      "https://i.picsum.photos/id/79/200/300.jpg?hmac=uqOMZrx9qlolrYp0xS5t84xjCiD_BIjfxIugTa1xjho"
+    )
+
+    this.myLastSnap = new FaceSnap(
+      "Blue Ocean",
+      "Ocean - Holidays 2016",
+      new Date(),
+      0,
+      "https://i.picsum.photos/id/147/200/200.jpg?hmac=Wk9sBW0wWCdCCp0HIczINX1TUZ_paAR_tCIKXcuYf2k"
+    )
+  }
 
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,28 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() faceSnap!: FaceSnap;
   buttonText! : string;
 
   ngOnInit() {
-    this.title = "Sahara Dune";
-    this.description = "Great Holidays in the Sahara";
-    this.createdDate = new Date();
-    this.snaps = 4;
-    this.imageUrl = "https://i.picsum.photos/id/261/200/200.jpg?hmac=4mNici_jSM7rVZzJL7M6G24f0axnDV3BZR-LBo5gMIg";
     this.buttonText = "Oh Snap!"
   }
 
   onSnap() {
     if (this.buttonText === "Oh Snap!") {
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.buttonText = "Oops Snap!"
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.buttonText = "Oh Snap!"
     }
   }
